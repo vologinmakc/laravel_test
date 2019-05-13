7044801504ed1b946310960005898551a6c614f1
Yuki_Rito <vologinmakc@gmail.com>
2019-05-13 19:52:15 +0700

    Create git

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,6 +4,9 @@
     <div class="row news-block">
         <div class="col-md-2"></div>
         <div class="col-md-8">
+            <div class="news-list-block">
+
+            </div>
         </div>
         <div class="col-md-2"></div>
     </div>
